af9f696baddecdca0da7428a2e671551c09e3805
Fedor <f.tkachov@gmail.com>
2023-08-26 13:14:01 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// --1-- //
+// Напишіть код, щоб вибрати елемент з атрибутом data-widget-name з документа
+// та прочитати його значення.
+// <!DOCTYPE html>
+// <html>
+// <body>
+// <div data-widget-name="menu">Виберіть жанр</div>
+// <script>/* ваш код */</script>
+// </body>
+// </html>
